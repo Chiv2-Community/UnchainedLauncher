93ef7dc03d3c309e6739997a3193f03512d8d9c6
Atom Roese <40679658+ARoese@users.noreply.github.com>
2023-09-10 13:57:23 -0400

    add .dll to function import

--- C2GUILauncher/src/Inject.cs
+++ C2GUILauncher/src/Inject.cs
@@ -15,7 +15,7 @@ namespace C2GUILauncher {
         [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
         public static extern IntPtr GetModuleHandle(string lpModuleName);
 
-        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
         static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
 
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]

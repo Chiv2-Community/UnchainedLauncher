49d8cf7a5221fdf60cd5dea3f3f5cffe6cc90049
Atom Roese <40679658+ARoese@users.noreply.github.com>
2024-05-31 22:54:41 -0400

    implement RCON functionality

---
+++ UnchainedLauncherCore/src/API/RCON.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnchainedLauncher.Core.API
+{
+    public class RCON
+    {
+        public static async Task sendCommand(IPEndPoint rconLocation, string command)
+        {
+            try
+            {
+                CancellationTokenSource cts = new(1000);
+                using TcpClient client = new TcpClient();
+                await client.ConnectAsync(rconLocation);
+                await client.GetStream().WriteAsync(
+                    (command+"\n").Map((c)=>(byte)c).ToArray(), // string -> char[]
+                    cts.Token
+                );
+            }
+            catch (OperationCanceledException)
+            {
+                throw new TimeoutException("RCON connection timed out");
+            }
+            
+        }
+    }
+}
---
+++ UnchainedLauncherCoreTests/src/API/RCONTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnchainedLauncher.Core.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace UnchainedLauncher.Core.API.Tests
+{
+    // make sure there is a Chivalry 2 server running
+    // RCON doesn't support sending anything back right now, so this can't return anything
+    // use wireshark and watch the game to make sure it's working
+    // Make sure the rcon blueprint is active. It will produce lots of console spam in-game if it is.
+    [TestClass()]
+    public class RCONTests
+    {
+        private static readonly IPEndPoint endpoint = IPEndPoint.Parse("127.0.0.1:9001");
+        [TestMethod()]
+        public async Task sendCommandTest()
+        {
+            await RCON.sendCommand(endpoint, "tbsaddstagetime 10");
+        }
+    }
+}

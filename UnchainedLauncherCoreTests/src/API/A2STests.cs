da1d5a9fd525cad278e3ca7efcad91c135939e84
Atom Roese <40679658+ARoese@users.noreply.github.com>
2024-08-11 17:10:26 -0400

    make and use MockA2s class for testing RegisteredServer.

--- UnchainedLauncherCoreTests/src/API/A2STests.cs
+++ UnchainedLauncherCoreTests/src/API/A2STests.cs
@@ -8,6 +8,37 @@ using System.Net;
 
 namespace UnchainedLauncher.Core.API.Tests
 {
+
+    public class MockA2S : IA2S
+    {
+        protected A2sInfo[] results;
+        protected int resultsPosition;
+        public int NumRequests { get { return resultsPosition; } }
+        public MockA2S()
+        {
+            results = new A2sInfo[1] { 
+                new(2, "Some Server Name", "ffa_Courtyard", "", "Chivalry 2",
+                    42, 1, 30, 0, ServerType.DEDICATED, Environment.WINDOWS, true, false)
+            };
+        }
+
+        /// <summary>
+        /// Initialize the mock a2s endpoint with an array of responses.
+        /// These responses will be returned by InfoAsync in order until exhausted. When
+        /// exhausted, the results will loop.
+        /// </summary>
+        /// <param name="a2SInfos"></param>
+        public MockA2S(A2sInfo[] a2SInfos)
+        {
+            results = a2SInfos;
+        }
+        public Task<A2sInfo> InfoAsync()
+        {
+            var res = results[resultsPosition++ % results.Length];
+            return Task.FromResult(res);
+        }
+    }
+
     // Should have a chivalry/A2S server running on 127.0.0.1:7071 for these to
     // test against
     [TestClass()]
--- UnchainedLauncherCoreTests/src/API/RegisteredServerTests.cs
+++ UnchainedLauncherCoreTests/src/API/RegisteredServerTests.cs
@@ -9,7 +9,7 @@ using System.Net;
 
 namespace UnchainedLauncher.Core.API.Tests
 {
-    // You must have a backend instance AND a chivalry 2 server running for these tests to succeed
+    // You must have a backend instance running for these tests to succeed
     [TestClass()]
     public class RegisteredServerTests
     {
@@ -23,11 +23,15 @@ namespace UnchainedLauncher.Core.API.Tests
         [TestMethod()]
         public async Task RegisterLoopTest()
         {
-            using RegisteredServer l = new(backend, testServerC2Info, "127.0.0.1");
+            // TODO: give this a custom series of responses and make sure the registered server behaves
+            // accordingly
+            MockA2S mockA2s = new();
+            using RegisteredServer l = new(backend, testServerC2Info, "127.0.0.1", a2sEndpoint: mockA2s);
             await Task.Delay(70000); //give time for a heartbeat to occur
             var reg = l.RemoteInfo;
             Assert.IsNotNull(reg);
             Assert.AreNotEqual(reg.UniqueId, "");
+            Assert.AreNotEqual(mockA2s.NumRequests, 0);
         }
     }
 }

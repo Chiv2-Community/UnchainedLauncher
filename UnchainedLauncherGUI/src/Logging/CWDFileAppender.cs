0fab82888316393c393882a46a9f38e349092d06
Jacob <jacoby6000@gmail.com>
2024-10-20 15:19:20 -0500

    More logging fixes

---
+++ UnchainedLauncherGUI/src/Logging/CWDFileAppender.cs
@@ -0,0 +1,12 @@
+using log4net.Appender;
+using System.IO;
+
+namespace UnchainedLauncher.GUI.Logging
+{
+    class CWDFileAppender : FileAppender
+    {
+        public override string? File {
+            set => base.File = value == null ? null : Path.Combine(Directory.GetCurrentDirectory(), value);
+        }
+    }
+}

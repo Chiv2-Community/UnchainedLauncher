54bb29b3422fc640545e182ce1e35e89e72e5e59
Jacob <jacoby6000@gmail.com>
2025-01-07 17:08:53 -0600

    Move DllInjection in to its own interfaces

---
+++ UnchainedLauncher.Core/Services/Processes/IProcessInjector.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+namespace UnchainedLauncher.Core.Services.Processes {
+    
+    /// <summary>
+    /// Injects processes with some alternate code, like a DLL.
+    /// </summary>
+    public interface IProcessInjector {
+        public bool Inject(Process p);
+    }
+}

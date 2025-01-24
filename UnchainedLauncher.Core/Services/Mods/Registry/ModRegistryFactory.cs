db1d0fa724c9acadb42b7ba89a153d12172e30fd
Jacoby6000 <2672832+Jacoby6000@users.noreply.github.com>
2025-01-24 17:21:58 +0000

    [GHA] Format Source

--- UnchainedLauncher.Core/Services/Mods/Registry/ModRegistryFactory.cs
+++ UnchainedLauncher.Core/Services/Mods/Registry/ModRegistryFactory.cs
@@ -6,28 +6,28 @@ namespace UnchainedLauncher.Core.Services.Mods.Registry {
     public class ModRegistryFactory {
         public static readonly JsonFactory<ModRegistryMetadata, IModRegistry> Instance =
             new JsonFactory<ModRegistryMetadata, IModRegistry>(ToClassType, ToJsonType);
-        
+
         public static IModRegistry ToClassType(ModRegistryMetadata metadata) =>
             metadata switch {
                 LocalModRegistryMetadata m => new LocalModRegistry(m.PakReleaseDir, ModRegistryDownloaderFactory.Instance.ToClassType(m.DownloaderMetadata)),
                 GithubModRegistryMetadata m => new GithubModRegistry(m.Org, m.RepoName, ModRegistryDownloaderFactory.Instance.ToClassType(m.DownloaderMetadata)),
                 AggregateModRegistryMetadata m => new AggregateModRegistry(m.Registries.Select(ToClassType).ToArray()),
-                
+
                 _ => throw new ArgumentOutOfRangeException(nameof(metadata), metadata,
                     $"Attempt to initialize unknown IModRegistry implementation: {metadata}")
             };
-        
+
         public static ModRegistryMetadata ToJsonType(IModRegistry registry) =>
             registry switch {
                 LocalModRegistry d => new LocalModRegistryMetadata(d.RegistryPath, ModRegistryDownloaderFactory.Instance.ToJsonType(d.ModRegistryDownloader)),
                 GithubModRegistry d => new GithubModRegistryMetadata(d.Organization, d.RepoName, ModRegistryDownloaderFactory.Instance.ToJsonType(d.ModRegistryDownloader)),
                 AggregateModRegistry d => new AggregateModRegistryMetadata(d.ModRegistries.Select(ToJsonType).ToArray()),
-                
+
                 _ => throw new ArgumentOutOfRangeException(nameof(registry), registry,
                     $"Attempt to extract metadata from unknown IModRegistry implementation: {registry}")
             };
     }
-    
+
     [UnionTag(nameof(Kind))]
     [UnionCase(typeof(LocalModRegistry), ModRegistryMetadataKind.LocalModRegistry)]
     [UnionCase(typeof(GithubModRegistry), ModRegistryMetadataKind.GithubModRegistry)]

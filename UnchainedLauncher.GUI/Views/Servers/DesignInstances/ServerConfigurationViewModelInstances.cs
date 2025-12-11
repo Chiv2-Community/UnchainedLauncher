c7e87dac768a69552054de17345a15a6ad555d19
Jacob <jacoby6000@gmail.com>
2025-12-11 13:06:37 -0600

    Reduce complexity of server configuration views

---
+++ UnchainedLauncher.GUI/Views/Servers/DesignInstances/ServerConfigurationViewModelInstances.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using UnchainedLauncher.Core.Services.Mods;
+using UnchainedLauncher.Core.Services.Mods.Registry;
+using UnchainedLauncher.GUI.ViewModels.ServersTab;
+
+namespace UnchainedLauncher.GUI.Views.Servers.DesignInstances {
+    public static class ServerConfigurationViewModelInstances {
+        public static ServerConfigurationVM DEFAULT => new ServerConfigurationDesignVM();
+    }
+
+    public class ServerConfigurationDesignVM : ServerConfigurationVM {
+        public ServerConfigurationDesignVM() : base(Mods.DesignInstances.ModListViewModelInstances.DEFAULTMODMANAGER)
+        {
+        }
+    }
+}

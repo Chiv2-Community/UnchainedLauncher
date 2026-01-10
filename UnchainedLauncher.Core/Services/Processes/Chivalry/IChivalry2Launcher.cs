ba6a5e3c14ec7aa6578afd313468fff73df352f3
Jacob Barber <jacoby6000@gmail.com>
2026-01-10 00:00:52 -0600

    Escape args

--- UnchainedLauncher.Core/Services/Processes/Chivalry/IChivalry2Launcher.cs
+++ UnchainedLauncher.Core/Services/Processes/Chivalry/IChivalry2Launcher.cs
@@ -2,6 +2,7 @@
 using LanguageExt.Common;
 using System.Diagnostics;
 using UnchainedLauncher.Core.Services.Mods.Registry;
+using UnchainedLauncher.Core.Utilities;
 
 namespace UnchainedLauncher.Core.Services.Processes.Chivalry;
 
@@ -115,7 +116,6 @@ public record ServerLaunchOptions(
         TDMTicketCount.IfSome(count => args.Add(new UEMapUrlParameter("TDMTicketCount", count.ToString())));
         PlayerBotCount.IfSome(count => args.Add(new UEMapUrlParameter("NumPlayerBots", count.ToString())));
         WarmupTime.IfSome(time => args.Add(new UEMapUrlParameter("WarmupTime", time.ToString())));
-
         return args;
     }
 };
@@ -125,7 +125,7 @@ public abstract record CLIArg(string Rendered);
 
 public record RawArgs(string Args) : CLIArg(Args);
 public record Flag(string FlagName) : CLIArg(FlagName);
-public record Parameter(string ParamName, string Value) : CLIArg($"{ParamName} \"{Value}\"");
+public record Parameter(string ParamName, string Value) : CLIArg($"{ParamName} \"{ArgumentEscaper.Escape(Value)}\"");
 public record UEParameter(string ParamName, string Value) : CLIArg($"{ParamName}={Value}");
 public record UEINIParameter(string Type, string Section, string Key, string Value) : CLIArg($"-ini:{Type}:[{Section}]:{Key}=\"{Value}\"");
 public record UEMapUrlParameter(string Key, string Value) : CLIArg($"?{Key}={Value}");
---
+++ UnchainedLauncher.Core/Utilities/ArgumentEscaper.cs
@@ -0,0 +1,10 @@
+﻿namespace UnchainedLauncher.Core.Utilities {
+    public static class ArgumentEscaper {
+        public static string Escape(string argument) {
+            return
+                argument
+                    .Replace("\"", "\\\"")
+                    .ReplaceLineEndings("\\r\\n");
+        }
+    }
+}

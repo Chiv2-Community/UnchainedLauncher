d05cd1a5dc47da9f9a3dffadb890365a82bbbc2b
Jacob Barber <jacoby6000@gmail.com>
2026-01-21 17:49:29 -0600

    Remove more isOrphaned

--- UnchainedLauncher.GUI/Views/Servers/DesignInstances/ServerViewModelInstances.cs
+++ UnchainedLauncher.GUI/Views/Servers/DesignInstances/ServerViewModelInstances.cs
@@ -71,8 +71,8 @@ namespace UnchainedLauncher.GUI.Views.Servers.DesignInstances {
             ),
             "Example Server",
             new ObservableCollection<MapDto>() {
-                new MapDto(Path: "", Hash: "", ClassPath: null, ObjectClass: null, IsOrphaned: null, GameMode: null, MapName: "FFA_Wardenglade", MapDescription: null, AttackingFaction: null, DefendingFaction: null, GamemodeType: null, TBLDefaultGameMode: null),
-                new MapDto(Path: "", Hash: "", ClassPath: null, ObjectClass: null, IsOrphaned: null, GameMode: null, MapName: "TDM_Wardenglade", MapDescription: null, AttackingFaction: null, DefendingFaction: null, GamemodeType: null, TBLDefaultGameMode: null)
+                new MapDto(Path: "", Hash: "", ClassPath: null, ObjectClass: null, GameMode: null, MapName: "FFA_Wardenglade", MapDescription: null, AttackingFaction: null, DefendingFaction: null, GamemodeType: null, TBLDefaultGameMode: null),
+                new MapDto(Path: "", Hash: "", ClassPath: null, ObjectClass: null, GameMode: null, MapName: "TDM_Wardenglade", MapDescription: null, AttackingFaction: null, DefendingFaction: null, GamemodeType: null, TBLDefaultGameMode: null)
             }
         ) {
             RestartCount = 2;

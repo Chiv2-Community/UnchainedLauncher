443e05e6f1d4dcd34b5bab30aca1abdcca1624eb
Jacob Barber <jacoby6000@gmail.com>
2026-01-21 18:09:16 -0600

    Make nullability of some fields reflect reality, update relevance checks

--- UnchainedLauncher.Core/Extensions/UnrealModScannerDtoExtensions.cs
+++ UnchainedLauncher.Core/Extensions/UnrealModScannerDtoExtensions.cs
@@ -9,13 +9,28 @@ public static class UnrealModScannerDtoExtensions {
             var lastDot = map.Path.LastIndexOf('.');
             return map.Path[(lastSlash + 1)..lastDot];
         }
+        
+        public bool IsRelevant() {
+            return !string.IsNullOrEmpty(map.GamemodeType?.Trim());
+        }
+    }
+
+    extension(BlueprintDto blueprintDto) {
+        public bool IsRelevant() {
+            var shouldBeHidden = blueprintDto.IsHidden ?? false;
+            var isClientSideOnly = blueprintDto.bClientside ?? false;
+            var isShownOnFrontend = blueprintDto.bAllowOnFrontend ?? false;
+            var hasShowInGUI = blueprintDto.bShowInGUI != null;
+            
+            return !shouldBeHidden && !isClientSideOnly && !isShownOnFrontend && hasShowInGUI;
+        }
     }
 
     extension(AssetCollections manifest) {
         public IEnumerable<BlueprintDto> RelevantBlueprints() =>
-            manifest.Blueprints.Where(x => (!x.IsHidden ?? false) && !x.bClientside);
+            manifest.Blueprints.Where(x => x.IsRelevant());
 
         public IEnumerable<MapDto> RelevantMaps() =>
-            manifest.Maps.Where(x => !string.IsNullOrEmpty(x.GamemodeType?.Trim()));
+            manifest.Maps.Where(x => x.IsRelevant());
     }
 }
